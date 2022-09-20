f48159858a84ae1cfab1fbd268ecb6d6ea35a455
KHONGTUB <jkojeda12@gmail.com>
2022-09-20 15:28:26 -0500

    switched back to browser router

--- src/App.js
+++ src/App.js
@@ -3,16 +3,16 @@ import "./App.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import Router from "./Router";
-import { HashRouter } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import Nav from "./containers/Nav";
 
 function App() {
   return (
     <Provider store={store}>
-      <HashRouter>
+      <BrowserRouter>
         <Nav />
         <Router />
-      </HashRouter>
+      </BrowserRouter>
     </Provider>
   );
 }
